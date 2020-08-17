d1a4d187609c08e72b064ca5ab5123ce91501470
edwardep <eduard3p@gmail.com>
2020-08-17 13:06:26 +0300

    testing generic aggregateFunc

--- src/test/java/misc/SlidingWindow.java
+++ src/test/java/misc/SlidingWindow.java
@@ -8,9 +8,12 @@ import datatypes.InternalStream;
 import datatypes.Vector;
 import operators.IncAggregation;
 import operators.WindowFunction;
+import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -28,12 +31,186 @@ import sources.SyntheticEventTimeSource;
 import test_utils.Testable;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.UUID;
 
 import static junit.framework.TestCase.assertEquals;
 
 
 public class SlidingWindow {
+
+
+    public interface BaseConfigDeduper<In, Acc> extends Serializable {
+        Acc createAcc();
+        Acc addAccs(In in, Acc acc);
+    }
+
+    public static class ConfigDeduper implements BaseConfigDeduper<Tuple2<String, Integer>, Tuple2<String, Integer>> {
+
+        @Override
+        public Tuple2<String, Integer> createAcc() {
+            return new Tuple2<>("0", 0);
+        }
+
+        @Override
+        public Tuple2<String, Integer> addAccs(Tuple2<String, Integer> in, Tuple2<String, Integer> acc) {
+            acc.f1 += in.f1;
+            return acc;
+        }
+    }
+
+    public static class GenericAggregateFunctionDeduper<In, Acc> implements AggregateFunction<In, Acc, Acc> {
+
+        BaseConfigDeduper<In, Acc> cfg;
+        public GenericAggregateFunctionDeduper(BaseConfigDeduper<In, Acc> cfg) {
+            this.cfg = cfg;
+        }
+
+        @Override
+        public Acc createAccumulator() {
+            return cfg.createAcc();
+        }
+
+        @Override
+        public Acc add(In in, Acc acc) {
+            return cfg.addAccs(in, acc);
+        }
+
+        @Override
+        public Acc getResult(Acc acc) {
+            return acc;
+        }
+
+        @Override
+        public Acc merge(Acc acc, Acc acc1) {
+            return null;
+        }
+    }
+
+
+    @Test
+    public void aggregateFunction_genericType() throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        DataStream<Tuple2<String,Integer>> source = env.fromElements(Tuple2.of("0",1), Tuple2.of("0",2), Tuple2.of("0",3));
+
+        source
+                .keyBy(k -> k.f0)
+                .countWindow(5, 1)
+                .aggregate(new GenericAggregateFunctionDeduper<>(new ConfigDeduper()), Types.TUPLE(Types.STRING,Types.INT), Types.TUPLE(Types.STRING,Types.INT))
+                .print();
+
+        env.execute();
+    }
+
+    //todo: make IncAggregator abstract so the used have to implement it
+
+    public interface MergedConfig<In, Acc, Out> extends BaseConfigAPI, AggregateFunction<In, Acc, Out> {}
+
+
+    public static class NewConfigAPI implements MergedConfig<Tuple2<String, Integer>, Tuple2<String, Integer>, Tuple2<String, Integer>> {
+
+        @Override
+        public Tuple2<String, Integer> createAccumulator() {
+            return new Tuple2<>("0", 0);
+        }
+
+        @Override
+        public Tuple2<String, Integer> add(Tuple2<String, Integer> in, Tuple2<String, Integer> acc) {
+            acc.f1 += in.f1;
+            return acc;
+        }
+
+        @Override
+        public Tuple2<String, Integer> getResult(Tuple2<String, Integer> acc) {
+            return acc;
+        }
+
+        @Override
+        public Tuple2<String, Integer> merge(Tuple2<String, Integer> stringIntegerTuple2, Tuple2<String, Integer> acc1) {
+            return null;
+        }
+    }
+
+
+    public interface BaseConfigAPI extends Serializable {
+        //These will be implemented directly from AggregateFunction
+        //Acc createAcc();
+        //Acc addAccumulators(In in, Acc acc);
+
+        //other methods to be overridden
+    }
+//
+//    public static class ConfigAPI implements BaseConfigAPI<Tuple2<String, Integer>, Tuple2<String,Integer>, Tuple2<String, Integer>> {
+//        @Override
+//        public Tuple2<String, Integer> createAcc() {
+//            return new Tuple2<>("0", 0);
+//        }
+//
+//        @Override
+//        public Tuple2<String, Integer> addAccumulators(Tuple2<String, Integer> in, Tuple2<String, Integer> acc) {
+//            acc.f1 += in.f1;
+//            return acc;
+//        }
+//
+//
+//    }
+
+
+//    public static class SimpleGenericAggregateFunc implements AggregateFunction< Tuple2< String, Integer >, Tuple2< String, Integer >, Tuple2< String, Integer > > {
+//
+//        private BaseConfigAPI<Tuple2<String, Integer>, Tuple2<String, Integer>, Tuple2<String, Integer>> cfg;
+//
+//        SimpleGenericAggregateFunc(BaseConfigAPI<Tuple2<String, Integer>, Tuple2<String, Integer>, Tuple2<String, Integer>> cfg) {
+//            this.cfg = cfg;
+//        }
+//
+//        @Override
+//        public Tuple2<String, Integer> createAccumulator() {
+//            return cfg.createAcc();
+//        }
+//
+//        @Override
+//        public Tuple2<String, Integer> add(Tuple2<String, Integer> in, Tuple2<String, Integer> acc) {
+//            return cfg.addAccumulators(in, acc);
+//        }
+//
+//        @Override
+//        public Tuple2<String, Integer> getResult(Tuple2<String, Integer> acc) {
+//            return acc;
+//        }
+//
+//        @Override
+//        public Tuple2<String, Integer> merge(Tuple2<String, Integer> a, Tuple2<String, Integer> b) {
+//            return a;
+//        }
+//    }
+
+//    public static class GenericAggregateFunc<In, Acc, Out> implements AggregateFunction<In, Acc, Out> {
+//
+//        private BaseConfigAPI<In, Acc> cfg;
+//        GenericAggregateFunc(BaseConfigAPI<In, Acc, Out> cfg) {
+//            this.cfg = cfg;
+//        }
+//        @Override
+//        public Acc createAccumulator() {
+//            return cfg.createAcc();
+//        }
+//        @Override
+//        public Acc add(In in, Acc acc) {
+//            return cfg.addAccumulators(in, acc);
+//        }
+//        @Override
+//        public Out getResult(Acc acc) {
+//            return (Out)acc;
+//        }
+//        @Override
+//        public Acc merge(Acc acc, Acc acc1) {
+//            return null;
+//        }
+//    }
+
 
     @Test
     public void slidingWindow_vector_diff_test() throws Exception {
